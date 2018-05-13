4116d22767b9a3f4709d5fc9472ab3d01e98412f
刘志贤 <771161641@qq.com>
2018-05-14 00:16:32 +0800

    init

---
+++ SourcetreeTest/SourcetreeTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SourcetreeTest
+//
+//  Created by 刘志贤 on 2018/5/14.
+//  Copyright © 2018年 刘志贤. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SourcetreeTest/SourcetreeTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SourcetreeTest
+//
+//  Created by 刘志贤 on 2018/5/14.
+//  Copyright © 2018年 刘志贤. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
